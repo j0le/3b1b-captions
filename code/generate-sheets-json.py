f01a1e7d47eed9e4573d4741c6dde6d9c18cffff
Ole <j0le@users.noreply.github.com>
2024-02-08 00:24:01 +0100

    Generate a sheets.json
    This JSON file contains one two-dimensional table, where each row
    correspons to a video. The first column contains the video identifier,
    and the colums after the first contain the names of contributors.
    
    The idea is, that is JSON file can later contain more tables, that can
    be exported to Google Sheets.
    
    It looks like this:
    
    ```
    {
      "contributors_per_video": [
        [
          "Video",
          "Contributors"
        ],
        [
          "2015/binary-counting",
          "Grant Sanderson",
          "Omer Tuchfeld",
          "bot224032",
          "luiz12apn"
        ],
        [
          "2015/eulers-characteristic-formula",
          "Grant Sanderson",
          "realcalal",
          "Omer Tuchfeld"
        ],
        [
          "2015/eulers-formula-old",
          "Grant Sanderson"
        ],
        // ...
      ]
    }
    ```

---
+++ code/generate-sheets-json.py
@@ -0,0 +1,31 @@
+import json
+import csv
+import pathlib
+import subprocess
+
+repo_root = pathlib.Path(__file__).parents[1]
+
+paths = sorted(repo_root.rglob("video_url.txt"))
+
+list_of_lines = [['Video','Contributors']]
+for path in paths:
+    if path.is_file():
+        folder = path.parent
+        relative_folder = folder.relative_to(repo_root)
+        proc = subprocess.run(["git",
+                        "shortlog", "--summary", "--numbered", "--", folder],
+                       capture_output=True, check=True, shell=False)
+        text = proc.stdout.decode("utf-8")
+        print(relative_folder.as_posix())
+        csv_line = [relative_folder.as_posix()]
+        for line in text.split('\n'):
+            fields = line.split('\t')
+            if len(fields) >= 2:
+                csv_line.append(fields[1])
+        list_of_lines.append(csv_line)
+
+json_text = json.dumps({
+            'contributors_per_video': list_of_lines
+        })
+
+(repo_root / "sheets.json").write_text(json_text)
