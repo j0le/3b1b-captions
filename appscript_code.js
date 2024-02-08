dd57abc52b542f28dc0a3cab119692fda1b05260
styrix560 <60841004+styrix560@users.noreply.github.com>
2024-02-08 14:55:34 +0100

    Update appscript_code.js

--- appscript_code.js
+++ appscript_code.js
@@ -3,6 +3,19 @@ function _onTrigger() {
   const rawData = response.getContentText();
   const data = JSON.parse(rawData);
 
+  const percentages = data["percentages"];
+  putDataIntoSheet("Percentages", percentages);
+  
+  const contributors = data["contributors"];
+  putDataIntoSheet("Contributors", contributors);
+}
+
+/**
+ * Takes data and puts it into the sheet with the specified name starting at cell A1. The data has to be a two dimensional array.
+ * @param {string} sheetName - The name of the sheet.
+ * @param {string[][]} author - The data to be written.
+ */
+function putDataIntoSheet(sheetName, data) {
   if (data.length == 0) return;
 
   const maxRowLength = Math.max(...data.map(row => row.length));
@@ -15,7 +28,7 @@ function _onTrigger() {
   }); // data filled with empty cells to become rectangular
 
   // write to sheet
-  const sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetName);
   const range = sheet.getRange(1, 1, data.length, maxRowLength);
   range.setValues(paddedData);
 }
