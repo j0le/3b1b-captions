d7bf8048efc10518f40d13c033036b7979d6bf55
styrix560 <guennie.pizzabauer@gmail.com>
2024-02-08 14:58:18 +0100

    update script to use json data in the correct format

--- code/main.py
+++ code/main.py
@@ -1,3 +1,8 @@
+import json
+
 with open("data.json", "w") as f:
-    f.write(
-        """[["name","some data","1234","more data"],["name with \\" and \\"\\"","and less columns "]]""")
+    data = {
+        "contributors": [["name", "some data", "1234", "56789"], ["name with \" and \"\"", "and less columns"]],
+        "percentages": [["another name", "some project idk", "58%"], ["max mustermann", "the same project", "a percentage that does not add up to a 100"]]
+    }
+    f.write(json.dumps(data))
