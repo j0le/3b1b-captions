b8bb1c123a655248389cd2b2b40da7e12c207230
styrix560 <guennie.pizzabauer@gmail.com>
2024-02-08 14:29:17 +0100

    escaping is hard

--- code/main.py
+++ code/main.py
@@ -1,3 +1,3 @@
 with open("data.json", "w") as f:
     f.write(
-        """[["name","some data","1234","more data"],["name with \" and \"\"","and less columns "]]""")
+        """[["name","some data","1234","more data"],["name with \\" and \\"\\"","and less columns "]]""")
