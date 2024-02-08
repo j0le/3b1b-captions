d5f1d5c8b15fc261e7e212d2be1f35013774ad8e
Ole <j0le@users.noreply.github.com>
2024-02-08 15:52:57 +0100

    Remove code/main.py

--- code/main.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open("data.json", "w") as f:
-    data = {
-        "contributors": [["name", "some data", "1234", "56789"], ["name with \" and \"\"", "and less columns"]],
-        "percentages": [["another name", "some project idk", "58%"], ["max mustermann", "the same project", "a percentage that does not add up to a 100"]]
-    }
-    f.write(json.dumps(data))
